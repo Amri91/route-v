5b80e77a0c28b5828b1926333c311ec1f9d83c2d
A. Amri <Amri91@users.noreply.github.com>
2018-01-05 11:07:56 +0100

    Incorporate semver (#8)
    * add semver

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,45 +1,57 @@
 'use strict';
 
-const {versionExtractor, v} = require('../lib');
+const V = require('../lib');
 
 describe('Middleware version control', () => {
   const getUrl = version => `segment/${version}/anotherVersion`;
+  let v;
 
-  describe('#version extractor', () => {
-    it('should extract the version number if found', () => {
-      expect(versionExtractor(getUrl('v1'))).toBe('1');
-    });
-    it('should return undefined if the version number was not found', () => {
-      expect(versionExtractor(getUrl())).toBe(undefined);
-    });
+  beforeEach(() => {
+    v = new V({});
   });
-  describe('#v', () => {
+
+  describe('#constructor', () => {
+    it('should throw an exception if versionPath is not an array', () => {
+      expect(() => new V({versionPath: 1})).toThrow();
+    });
+    it('should expect empty params', () => {
+      expect(() => new V()).not.toThrow();
+    })
+  });
+  describe('#register', () => {
     let versions;
     beforeEach(() => {
-      versions = {default: jest.fn(), 0: jest.fn(), 1: jest.fn()};
+      versions = {'0.0.1': jest.fn(), '>0.0.1': jest.fn(), '*': jest.fn()};
     });
     it('should call the correct version', () => {
-      v(versions)({url: getUrl('v0')});
-      expect(versions[0].mock.instances.length).toBe(1);
+      v.register(versions)({url: getUrl('v0.2.1')});
+      expect(versions['>0.0.1'].mock.instances.length).toBe(1);
     });
-    it('should call the default function if version was not found listed', () => {
-      v(versions)({url: getUrl('v2')});
-      expect(versions.default.mock.instances.length).toBe(1);
+    it('should call the wild card version if nothing else matched (order is preserved)', () => {
+      v.register(versions)({url: getUrl('v0.0.0')});
+      expect(versions['*'].mock.instances.length).toBe(1);
+    });
+    it('should throw an error if nothing was matched', () => {
+      expect(() => v.register({'0.0.0': jest.fn()})({url: getUrl('v0.0.1')})).toThrow();
     });
     it('should use the custom path provided to get the version', () => {
-      const newV = v(undefined, [0, 'custom']);
-      newV(versions)({custom: getUrl('v0')});
-      expect(versions[0].mock.instances.length).toBe(1);
+      const newV = new V({versionPath: [0, 'custom']});
+      newV.register(versions)({custom: getUrl('v0.0.1')});
+      expect(versions['0.0.1'].mock.instances.length).toBe(1);
     });
     it('should use the new extractor', () => {
-      const newV = v(() => 1, undefined);
-      newV(versions)({url: getUrl('v0')});
-      expect(versions[1].mock.instances.length).toBe(1);
-    });
-    it('should use the new extractor (without calling second parameter)', () => {
-      const newV = v(() => 1);
-      newV(versions)({url: getUrl('v0')});
-      expect(versions[1].mock.instances.length).toBe(1);
+      const newV = new V({versionExtractor: () => '20.0.0'});
+      newV.register(versions)({url: getUrl('_')});
+      expect(versions['>0.0.1'].mock.instances.length).toBe(1);
     });
   });
+  describe('#versionChecker[k]', () => {
+    it('If k is satisfies, it should pass if version satisfied the predicate ^1.0.0', (done) => {
+      v = new V({globalVersionChecker: (isSatisfied, details) => (ctx, next) => {
+        expect(details).toBeTruthy();
+        done(!isSatisfied);
+      }});
+      v.versionChecker.satisfies('^1.0.0')({url: getUrl('v1.0.1')});
+    })
+  })
 });
